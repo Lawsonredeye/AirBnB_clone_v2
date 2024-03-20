911b6a55f65698f39d7f4489e6aa63ace82406bd
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-21 00:03:06 +0100

    misspelt exists

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,9 @@
 -- Creates a Mysql server for the project
 -- with a user and authentication
-CREATE DATABASE IF NOT EXITS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- creating server user
-CREATE USER IF NOT EXITS 'hbnb_test'@'localhost'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant privileges to the necessary DB
