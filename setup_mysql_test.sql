99fa4b9efc7cdd5873d4627d361221e8af075b79
lawsonredeye <omoregbeeolawson@gmail.com>
2024-03-20 23:59:47 +0100

    created a test database for testing codes

---
+++ setup_mysql_test.sql
@@ -0,0 +1,19 @@
+-- Creates a Mysql server for the project
+-- with a user and authentication
+CREATE DATABASE IF NOT EXITS hbnb_test_db;
+
+-- creating server user
+CREATE USER IF NOT EXITS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to the necessary DB
+GRANT ALL PRIVILEGES
+ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+
+GRANT SELECT
+ON performance_schema.*
+TO 'hbnb_test'@'localhost';
+
+-- Reload instruction into memory
+FLUSH PRIVILEGES;
