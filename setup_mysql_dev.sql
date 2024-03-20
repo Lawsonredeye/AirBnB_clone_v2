695cdee98b6d5071fa27ccb45e533f5205fab9a9
enejivk <enejivic@gmail.com>
2024-03-20 23:45:56 +0100

    mysql database create

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,17 @@
+-- Creating database and a user
+-- Granting all permission to a database and only select to the performance schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES
+ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
+
+GRANT SELECT
+ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
