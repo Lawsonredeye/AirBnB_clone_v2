ec1ad9a33515d8920780f723bfde2f48935d3e8a
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-04 22:50:33 +0100

    worked on script to configure server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,11 @@ check=$(which nginx)
 if [ -z "$check" ]
 then
 	sudo apt install nginx
+else
+	echo "Already installed"
 fi 
 
-content="
+content="\
 <html>
   <head>
   </head>
@@ -23,14 +25,14 @@ cd releases; mkdir test; echo "$content" > index.html
 
 # - create a symbolic link from test to current
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
 # sudo sed -i 's/root /var/www/html;/ alias /data/web_static/current/;/g' /etc/nginx/sites-enabled/default
-#
+# sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i 's#root /var/www/html;#root /var/www/html;\n        location /hbnb_static/ {\n            alias /data/web_static/current/;\n        }#' /etc/nginx/sites-enabled/default
 
 # - Restart Server
 sudo service nginx restart
