9904f9d7ded5929e080bab5e0c718e5dabaf53c9
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-18 19:29:54 +0100

    (Fix): deleted some stuffs

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,11 +32,10 @@ def do_deploy(archive_path):
     archive = archive_path[:-4]
     new_path = f"/data/web_static/releases/{archive}"
     try:
-        run('mkdir -p /tmp/versions')
-        put(archive_path, '/tmp/versions')
+        put(archive_path, '/tmp/')
         run(f'mkdir -p /data/web_static/releases/{archive}')
         run(f'tar -xvf /tmp/{archive_path} -C {new_path}')
-        run(f'rm -rf /tmp/versions/{archive_path}')
+        run(f'rm -rf /tmp/{archive_path}')
         run(f'rm -rf /data/web_static/current')
         run(f'mkdir -p /data/web_static/current')
         run(f'ln -s /data/web_static/current {new_path}')
