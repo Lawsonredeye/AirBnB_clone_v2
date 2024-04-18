c66023e29d66a0f34ab074de35a64e81299ef9d8
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-18 15:39:14 +0100

    (Fix): created the /tmp/versions before deployment and then put the archive into the /tmp/versions on the remote server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,10 +33,10 @@ def do_deploy(archive_path):
     new_path = f"/data/web_static/releases/{archive}"
     try:
         run('mkdir -p /tmp/versions')
-        put(archive_path, '/tmp/')
+        put(archive_path, '/tmp/versions')
         run(f'mkdir -p /data/web_static/releases/{archive}')
         run(f'tar -xvf /tmp/{archive_path} -C {new_path}')
-        run(f'rm -rf /tmp/{archive_path}')
+        run(f'rm -rf /tmp/versions/{archive_path}')
         run(f'rm -rf /data/web_static/current')
         run(f'mkdir -p /data/web_static/current')
         run(f'ln -s /data/web_static/current {new_path}')
