8366e432c1063dd6ac6fef3eff1465eb934c0b25
lawsonredeye <omoregbeeolawson@gmail.com>
2024-04-21 01:05:43 +0100

    (Fix): new line after function

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,10 +14,12 @@ def hello():
     """Just returns Hello"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def hbnb():
     """print HBNB"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
